a56667d7124f1bf9eb212764f722b0a196ff43f2
Casey Heidrich <crheidri@users.noreply.github.com>
2018-10-23 16:57:33 -0600

    search_env see also

--- search_env.m
+++ search_env.m
@@ -2,6 +2,8 @@ function value = search_env(varargin)
 %SEARCH_ENV search for environment variables.
 %   SEARCH_ENV(VAR1,VAR2,...) searches for system environment variables
 %   VAR1,VAR2,... and returns the first non-empty value.
+%
+%   See also getenv.
 
 for k=1:length(varargin)
     value = getenv(varargin{k});
